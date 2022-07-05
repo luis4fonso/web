883c6ff5e0958b9a4d69fab5196da7b86de7b81a
Raphael <phael92br@gmail.com>
2022-07-05 02:27:22 -0300

    adicionando back-end

---
+++ models/Item_Pedido.js
@@ -0,0 +1,54 @@
+module.exports = (sequelize,DataTypes) => {
+    let itemPedido = sequelize.define(
+        'Item_Pedido',
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                allowNull: false,
+                primaryKey: true
+            },
+            quantidade: {
+                type: DataTypes.TINYINT(1),
+                allowNull: true
+            },
+            pedidos_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                  model:{
+                    tableName: "pedidos"
+                  },
+                  key:"id"
+                }
+            },    
+            usuarios_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                  model:{
+                    tableName: "usuarios"
+                  },
+                  key:"id"
+                }
+            },
+            produtos_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                  model:{
+                    tableName: "produtos"
+                  },
+                  key:"id"
+                }
+            }
+        },
+        {
+            tableName: 'itens_pedidos',
+            timestamps: true,
+        }
+    )
+    itemPedido.associate = (models) => {}
+
+    return itemPedido;
+}
